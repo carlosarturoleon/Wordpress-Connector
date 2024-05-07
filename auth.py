3feee2856b6997113ab10434c04bd6bd55f97304
carlosarturoleon <ctr486@yahoo.com>
2024-05-07 00:44:03 -0500

    Remove token generation function from auth.py
    The function responsible for generating the token has been removed from auth.py as it now resides in a separate dedicated file. This change improves modularity and code organization.

--- auth.py
+++ auth.py
@@ -27,28 +27,3 @@ def oauth_callback():
     else:
         return "No code provided by WordPress."
 
-
-def get_access_token(client_id, redirect_uri, client_secret, code):
-    """
-    Function to get an access token from WordPress.
-
-    Args:
-    client_id (str): Your application's client ID.
-    redirect_uri (str): The redirect URI for your application.
-    client_secret (str): Your application's client secret key.
-    code (str): The code received from the authorization request.
-
-    Returns:
-    dict: A dictionary containing the access token and related information.
-    """
-    token_url = "https://public-api.wordpress.com/oauth2/token"
-    data = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
-        "client_secret": client_secret,
-        "code": code,
-        "grant_type": "authorization_code",
-    }
-
-    response = requests.post(token_url, data=data)
-    return response.json()
